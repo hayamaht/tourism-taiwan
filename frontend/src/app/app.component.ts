b74cdf3f23791e1a04ffd21541ed0b82521e7e53
hayama <hayamaroom@gmail.com>
2023-10-21 14:22:13 +0800

    Add footer component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 <div class="m-4">
   <router-outlet></router-outlet>
 </div>
+<app-footer></app-footer>
   `,
   imports: [
-    CommonModule, RouterOutlet, NavbarComponent,
+    CommonModule, RouterOutlet,
+    NavbarComponent, FooterComponent
   ],
 })
 export class AppComponent {}
